179cc784664fc73956db318287a94c9a84943bc1
landonkail <landon.kail@gmail.com>
2015-10-28 11:36:46 -0400

    add test data

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,13 +13,38 @@ public class Main {
         HashMap<String, User> users = new HashMap<>();
         ArrayList<Message> messages = new ArrayList<>();
 
+        addTestMessages(users);
+        addTestMessages(messages);
+
         Spark.get(
                 "/",
                 ((request, response) -> {
+                    ArrayList<Message> threads = new ArrayList<Message>(); //List that is only top level threads
+                    for (Message message : messages) {
+                        if (message.replyId == -1) {
+                            threads.add(message);
+                        }
+                    }
+
                     HashMap m = new HashMap();
+                    m.put("threads", threads);  // Adding only top level threads
                     return new ModelAndView(m, "threads.html");
                 }),
                 new MustacheTemplateEngine()
         );
+    }
+
+    static void addTestUsers(HashMap<String, User> users) {
+        users.put("Alice", new User());
+        users.put("Bob", new User());
+        users.put("Charlie", new User());
+    }
+
+    static void addTestMessages(ArrayList<Message> messages) {
+        messages.add(new Message(0, -1, "Alice", "This is a thread!"));
+        messages.add(new Message(1, -1, "Bob", "This is a thread!"));
+        messages.add(new Message(2, 0, "Charlie", "Cool thread, Alice"));
+        messages.add(new Message(3, 2, "Alice", "Thanks"));
+
     }
 }
